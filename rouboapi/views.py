958415af913a6a0b4cb5735cc01368387e43c726
junjian <junjian_11@163.com>
2018-12-03 16:38:58 +0800

    整理现有代码，去除影响性能的数据获取
    Change-Id: Iee51ef611e13bfe0cfa4d0928228501b98a018a1

--- rouboapi/views.py
+++ rouboapi/views.py
@@ -32,6 +32,7 @@ from bs4 import BeautifulSoup
 import bs4
 from selenium import webdriver
 
+
 # sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 
 
@@ -195,9 +196,15 @@ class OpenCard(APIView):
         except:
             return None
 
+    # --------------------------------------------------------------------
+    # openCard 搜索接口
+    # 接口特点：各种聚合内容的搜索方式不同，基本上是基于 api 和基于网页实时爬取两种
+    #          搜索结果只用于前端展示，提供给用户选择，所以入库，也不获取复杂的详细信息
+    # --------------------------------------------------------------------
+
     def searchJueJinByGoogle(self, key):
         """
-        通过 google 自定义搜索获取掘金账号信息
+        通过 google 自定义搜索获取掘金账号信息(次数受限，每天仅100次）
         :param key:
         :return:
         """
@@ -226,6 +233,11 @@ class OpenCard(APIView):
             return None
 
     def searchGitHub(self, key):
+        """
+        通过 github 公开 api ，无访问限制
+        :param key:
+        :return:
+        """
         result = []
         URL = 'https://api.github.com/search/users?q=' + key
         try:
@@ -237,6 +249,12 @@ class OpenCard(APIView):
             return result
 
     def searchJianShuByH5(self, key):
+        """
+        通过实时爬取简书搜索用户页面获取信息，速度慢，但是为了不浪费一次爬虫数据，便尽可能多地获取有用数据
+        （仅爬取一个页面）
+        :param key:
+        :return:
+        """
         result = []
         URL = 'https://www.jianshu.com/search?page=1&type=user&q=' + key
         option = webdriver.ChromeOptions()
@@ -252,6 +270,7 @@ class OpenCard(APIView):
             res = {}
             for child in info.children:
                 if isinstance(child, bs4.element.Tag):
+                    """ 由于简书阅读量的爬取速度慢，严重影响 api 质量，所以暂时先停止该数据的获取。后期改用异步爬取的方式优化
                     if child.attrs and 'href' in child.attrs:
                         res['uid'] = child.attrs['href'].split('/')[2]
                         res['totalViews'] = 0
@@ -273,6 +292,7 @@ class OpenCard(APIView):
                                                 pass
                             if count < 9:
                                 break
+                    """
                     for ch in child.children:
                         if isinstance(ch, bs4.element.Tag):
                             for chch in ch.children:
@@ -290,7 +310,21 @@ class OpenCard(APIView):
             driver.close()
         return result
 
+    # --------------------------------------------------------------------
+    # openCard  详情数据获取接口
+    # 接口特点：各种聚合内容的详情数据方式也不同，还是基于 api 和基于网页实时爬取两种
+    #          详情数据目前有三种类型：
+    #          1、可以实时获取的数据，该类接口先获取最新数据后，入库，后吐给客户端
+    #          2、非实时获取的数据，直接从库中读出，返回客户端，数据的更新又后台定时进行
+    #          3、第三方的 api 稳定、实时性好的详情数据，不入库保持，直接返回给客户端
+    # --------------------------------------------------------------------
     def getJianShuInfoByH5(self, openid, uid):
+        """
+        （实时获取）简书详情信息，爬虫实现
+        :param openid:
+        :param uid:
+        :return:
+        """
         if OpenCards.objects.filter(openid=openid):
             query = OpenCards.objects.get(openid=openid)
             bskeys = model_to_dict(query)['bskeys']
@@ -303,15 +337,15 @@ class OpenCard(APIView):
             option.add_argument('headless')
             option.add_argument('--headless')
             option.add_argument('--disable-gpu')
-            driver = webdriver.Chrome(chrome_options=option, executable_path='/data/bin/Downloads/chromedriver')
+            driver = webdriver.Chrome(chrome_options=option, executable_path='/data/bin/chromedriver')
             for page in range(1, 100):
                 driver.get('https://www.jianshu.com/u/' + uid + '?order_by=shared_at&page=' + str(page))
                 soup = BeautifulSoup(driver.page_source, 'html.parser')
                 # 获取用户信息
                 if 'followers' not in bskeys['jianshu']:
-                    title = soup.find('div', attrs={'class':'title'})
+                    title = soup.find('div', attrs={'class': 'title'})
                     bskeys['jianshu']['name'] = title.a.string.strip()
-                    info = soup.find('div', attrs= {'class':'info'})
+                    info = soup.find('div', attrs={'class': 'info'})
                     count = 0
                     for ch in info.children:
                         if isinstance(ch, bs4.element.Tag):
@@ -324,9 +358,13 @@ class OpenCard(APIView):
                                     if count == 3:
                                         bskeys['jianshu']['totalCollections'] = '写了' + chch.div.p.string.strip() + '字,'
                                     if count == 4:
-                                        bskeys['jianshu']['totalCollections'] = '获得了' + chch.div.p.string.strip() + '个喜欢'
+                                        bskeys['jianshu'][
+                                            'totalCollections'] = '获得了' + chch.div.p.string.strip() + '个喜欢'
                                     count += 1
-                # 获取阅读数
+                else:
+                    # 不再获取阅读量，故直接返回
+                    break
+                """ 由于简书阅读量的爬取速度慢，严重影响 api 质量，所以暂时先停止该数据的获取。后期改用异步爬取的方式优化
                 tags = soup.find('ul', attrs={'class': 'note-list'})
                 count = 0
                 for ch in tags:
@@ -335,13 +373,15 @@ class OpenCard(APIView):
                             if isinstance(chch, bs4.element.Tag):
                                 try:
                                     for chchch in chch.div.a:
-                                        if isinstance(chchch, bs4.element.NavigableString) and chchch.string.strip() != '':
+                                        if isinstance(chchch,
+                                                      bs4.element.NavigableString) and chchch.string.strip() != '':
                                             bskeys['jianshu']['totalViews'] += int(chchch.string.strip())
                                             count += 1
                                 except:
                                     pass
                 if count < 9:
                     break
+                """
             bskeys['jianshu']['uid'] = uid
             OpenCards.objects.filter(openid=openid).update(bskeys=str(bskeys).strip())
             driver.close()
@@ -350,7 +390,7 @@ class OpenCard(APIView):
 
     def getGitHubInfo(self, openid, name):
         """
-        获取具体的github用户信息
+        (实时获取）获取具体的github用户信息，不入库
         :param uid:
         :return:
         """
@@ -366,7 +406,7 @@ class OpenCard(APIView):
 
     def getAndSaveJueJinInfo(self, openid, uid):
         """
-        获取具体的掘金用户信息
+        实时获取具体的掘金用户信息（接口）
         :param uid:
         :return:
         """
@@ -396,7 +436,19 @@ class OpenCard(APIView):
         else:
             return False
 
+    # --------------------------------------------------------------------
+    # openCard  保存用户选择接口
+    # 接口特点: 只保存关键数据，与 openid 做好映射
+    # --------------------------------------------------------------------
     def saveConnectInfo(self, openid, phone, email, name):
+        """
+         保存联系方式信息
+        :param openid:
+        :param phone:
+        :param email:
+        :param name:
+        :return:
+        """
         if OpenCards.objects.filter(openid=openid):
             query = OpenCards.objects.get(openid=openid)
             bskeys = model_to_dict(query)['bskeys']
@@ -414,6 +466,12 @@ class OpenCard(APIView):
             return False
 
     def saveJianShuInfo(self, openid, info):
+        """
+        保存用户简书信息
+        :param openid:
+        :param info:
+        :return:
+        """
         if OpenCards.objects.filter(openid=openid):
             query = OpenCards.objects.get(openid=openid)
             bskeys = model_to_dict(query)['bskeys']
@@ -429,6 +487,13 @@ class OpenCard(APIView):
             return False
 
     def saveGitHubInfo(self, openid, id, name):
+        """
+        保存 github 用户信息
+        :param openid:
+        :param id:
+        :param name:
+        :return:
+        """
         if OpenCards.objects.filter(openid=openid):
             query = OpenCards.objects.get(openid=openid)
             bskeys = model_to_dict(query)['bskeys']
@@ -445,9 +510,17 @@ class OpenCard(APIView):
             return False
 
     def get(self, request, format=None):
+        """
+        接口入口：待规范
+        :param request:
+        :param format:
+        :return:
+        """
         req = request.query_params
+        # 通用参数检测
         if 'type' not in req:
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        # 微信用户鉴权
         if req['type'] == 'login' and 'wxcode' in req:
             res = self.wxCode2Session(req['wxcode'])
             if not res or 'openid' not in res:
@@ -463,42 +536,49 @@ class OpenCard(APIView):
                     if serializer.is_valid():
                         serializer.save()
                 return Response({"data": {"openid": openid}}, status=status.HTTP_200_OK)
+        # 搜索掘金
         elif req['type'] == 'search' and 'from' in req and req['from'] == 'juejin' and 'key' in req:
             res = self.searchJueJinByGoogle(req['key'])
             if res:
                 return Response({"data": res}, status=status.HTTP_200_OK)
             else:
                 return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 搜索简书
         elif req['type'] == 'search' and 'from' in req and req['from'] == 'jianshu' and 'key' in req:
             res = self.searchJianShuByH5(req['key'])
             if res:
                 return Response({"data": res}, status=status.HTTP_200_OK)
             else:
                 return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 搜索github
         elif req['type'] == 'search' and 'from' in req and req['from'] == 'github' and 'key' in req:
             res = self.searchGitHub(req['key'])
             if res:
                 return Response({"data": res}, status=status.HTTP_200_OK)
             else:
                 return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 保存github
         elif req['type'] == 'save' and 'from' in req and 'openid' in req and req['from'] == 'github':
             res = self.saveGitHubInfo(req['openid'], req['id'], req['login'])
             if res:
                 return Response({"data": []}, status=status.HTTP_200_OK)
             else:
                 return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 保存掘金
         elif req['type'] == 'save' and 'from' in req and 'openid' in req and req['from'] == 'juejin':
             res = self.getAndSaveJueJinInfo(req['openid'], req['uid'])
             if res:
                 return Response({"data": []}, status=status.HTTP_200_OK)
             else:
                 return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 保存简书
         elif req['type'] == 'save' and 'from' in req and 'openid' in req and req['from'] == 'jianshu':
             res = self.saveJianShuInfo(req['openid'], req['info'])
             if res:
                 return Response({"data": []}, status=status.HTTP_200_OK)
             else:
                 return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 保存联系方式
         elif req['type'] == 'save' and 'from' in req and 'openid' in req and req['from'] == 'connect':
             try:
                 res = self.saveConnectInfo(req['openid'], req['phone'], req['email'], req['name'])
@@ -508,6 +588,7 @@ class OpenCard(APIView):
                     return Response({'data': {}}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             except:
                 return Response({'data': {}}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 获取掘金详情
         elif req['type'] == 'bskeys' and 'openid' in req and 'from' in req and req['from'] == 'juejin':
             if OpenCards.objects.filter(openid=req['openid']):
                 try:
@@ -525,6 +606,7 @@ class OpenCard(APIView):
                     return Response({"data": {}}, status=status.HTTP_200_OK)
             else:
                 return Response({"data": {}}, status=status.HTTP_200_OK)
+        # 获取联系方式详情
         elif req['type'] == 'bskeys' and 'openid' in req and 'from' in req and req['from'] == 'connect':
             if OpenCards.objects.filter(openid=req['openid']):
                 try:
@@ -537,6 +619,7 @@ class OpenCard(APIView):
                     return Response({"data": {}}, status=status.HTTP_200_OK)
             else:
                 return Response({"data": {}}, status=status.HTTP_200_OK)
+        # 获取简书详情
         elif req['type'] == 'bskeys' and 'openid' in req and 'from' in req and req['from'] == 'jianshu':
             if OpenCards.objects.filter(openid=req['openid']):
                 try:
@@ -551,9 +634,10 @@ class OpenCard(APIView):
                     data['bskeys'] = eval(data['bskeys'])
                     return Response({"data": data['bskeys']['jianshu']}, status=status.HTTP_200_OK)
                 except:
-                   return Response({"data": {}}, status=status.HTTP_200_OK)
+                    return Response({"data": {}}, status=status.HTTP_200_OK)
             else:
                 return Response({"data": {}}, status=status.HTTP_200_OK)
+        # 获取github详情
         elif req['type'] == 'bskeys' and 'openid' in req and 'from' in req and req['from'] == 'github':
             if OpenCards.objects.filter(openid=req['openid']):
                 try:
@@ -564,6 +648,6 @@ class OpenCard(APIView):
                     res = self.getGitHubInfo(req['openid'], data['bskeys']['github']['login'])
                     return Response({"data": res}, status=status.HTTP_200_OK)
                 except:
-                   return Response({"data": {}}, status=status.HTTP_200_OK)
+                    return Response({"data": {}}, status=status.HTTP_200_OK)
             else:
                 return Response({"data": {}}, status=status.HTTP_200_OK)
